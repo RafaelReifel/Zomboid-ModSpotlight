3075feb23a670d5e34dea2432bdc736ad23e9cf8
RafaelReifel <44376873+RafaelReifel@users.noreply.github.com>
2024-03-17 11:31:47 -0300

    Update update.lua

--- update.lua
+++ update.lua
@@ -1,3 +1,5 @@
 ModList = {
 firetrail=clear corpses with gasoline trail on fire
-vhs in cars CMP= watch vhs inside and near vehicles
+vhs in cars CMP=watch vhs inside and near vehicles
+Wheelbarrow=move logs (items) with craftable two-handed car
+DragonRadar=find a list of items on map
